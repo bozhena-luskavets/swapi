ae45d9fc50cee13456221d6c1fc2e14d0a75cc28
DESKTOP-SJJ9J08\Bozhena <luskavetsbozhena@gmail.com>
2022-11-08 13:25:45 +0200

    update loader files

--- src/Components/Loader/index.js
+++ src/Components/Loader/index.js
@@ -1,62 +1,10 @@
-import styled, { keyframes } from 'styled-components';
+import React from 'react';
+import { Container, LoaderItem } from './style';
 
 export const Loader = () => {
-
-    return <Container>
-        <LoaderItem />
-    </Container>
+    return (
+        <Container>
+            <LoaderItem />
+        </Container>
+    )
 }
-
-
-
-
-
-const mvx = keyframes`
-    0%, 10%  {transform: translate(0, 0) rotate(0deg)}
-    30% {transform: translate(-50%, -50%) rotate(0deg)}
-    50% {transform: translate(-50%, -50%) rotate(180deg)}
-    75%, 100% {transform: translate(0, 0) rotate(180deg)}
-`;
-
-const mvrx = keyframes`
-    0%, 10%  {transform: translate(0, 0) rotate(0deg)}
-    30% {transform: translate(50%, 50%) rotate(0deg)}
-    50% {transform: translate(50%, 50%) rotate(180deg)}
-    75%, 100% {transform: translate(0, 0) rotate(180deg)}
-`;
-
-const LoaderItem = styled.div`
-    width: 47px;
-    height: 47px;
-    position: relative;
-    background-color: var(--global-bg);
-
-
-    ::before, ::after {
-        content:"";
-        display: block;
-        border: 24px solid;
-        border-color: transparent transparent #fff #fff;
-        position: absolute;
-        left: 0;
-        top: 0;
-        animation: ${mvx} 1.2s infinite ease-in;
-    }
-
-    ::before {
-        border-color: #fff  #fff transparent transparent;
-        animation-name: ${mvrx};
-    }
-`;
-
-const Container = styled.div`
-    width: 100%;
-    height: 100vh;
-    background-color: transparent;
-    position: fixed;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    top: 0;
-    left: 0;
-`
